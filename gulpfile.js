dbf0b09fc220a1cabd66f98f5e50b19590185056
Rafael Costa <rafaelsevla@gmail.com>
2021-09-02 14:53:42 -0300

    fix pre commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ const { src, dest, series } = require('gulp');
 var concat = require('gulp-concat');
 
 
-exports.default = function() {
+exports.default = function () {
   return src('dist/*.js').pipe(concat('remoteEntry.js')).pipe(dest('../shell/dist/mfe2/'));
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
+  entry: './src/index',
+  mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     port: 3003,
   },
   output: {
-    publicPath: "auto",
+    publicPath: 'auto',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [ '.ts', '.tsx', '.js' ],
   },
   module: {
     rules: [
       {
         test: /bootstrap\.tsx$/,
-        loader: "bundle-loader",
+        loader: 'bundle-loader',
         options: {
           lazy: true,
         },
       },
       {
         test: /\.tsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-react", "@babel/preset-typescript"],
+          presets: [ '@babel/preset-react', '@babel/preset-typescript' ],
         },
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "mfe2",
-      filename: "remoteEntry.js",
+      name: 'mfe2',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./Widget": "./src/Widget",
+        './Widget': './src/Widget',
       },
       shared: [
-        { react: { singleton: true }, "react-dom": { singleton: true } },
-        "moment",
+        { react: { singleton: true }, 'react-dom': { singleton: true } },
+        'moment',
       ],
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
